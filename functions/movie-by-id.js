afa0ee8198d4f2aca862d49356ce942ba20610d3
Bill Keller <bill.keller1@toyota.com>
2022-04-13 14:20:02 -0400

    Initial FEM setup

---
+++ functions/movie-by-id.js
@@ -0,0 +1,15 @@
+const movies = require('../data/movies.json');
+
+exports.handler = async ({ queryStringParameters }) => {
+  const { id } = queryStringParameters;
+  const movie = movies.find((currentMovie) => currentMovie.id === id);
+
+  if (!movie) {
+    return { statusCode: 404, body: 'Not Found' };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(movie),
+  };
+};
